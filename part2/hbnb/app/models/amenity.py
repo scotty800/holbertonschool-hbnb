6df547df1adeb65ed40dd0b91304216755e66416
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-16 13:20:59 +0200

    create user and amenity

---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,9 @@
+from models.BaseModel import BaseModel
+class Amenity(BaseModel):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+        if len(name) > 50:
+            raise ValueError("The equipment name cannot exceed 50 characters.")
+
---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,22 @@
+from models.BaseModel import BaseModel
+class user(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+
+        if len(first_name) > 50:
+            raise ValueError("First name should not exceed 50 characters")
+        if len(last_name) > 50:
+            raise ValueError ("First name should not exceed 50 characters")
+
+    def format_email(self, email):
+        if not isinstance(email, str):
+            raise ValueError ("Invalid email format")
+
+    def is_valide_email(self, email):
+        if '@' in email and "." in email.split('@')[-1]:
+            return True
+        return False
