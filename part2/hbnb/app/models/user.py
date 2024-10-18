62e2de95e4eeb82cd6ffb7cadba4c96862a3b626
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-18 18:04:28 +0200

    add user API

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,21 +1,27 @@
 import re
-from base_models import BaseModel
+from .base_models import BaseModel
 
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
 
         if len(first_name) > 50:
             raise ValueError("First name should not exceed 50 characters")
         if len(last_name) > 50:
             raise ValueError ("First name should not exceed 50 characters")
-        if not self.is_valid_email(email):
-            raise ValueError("Invalid email format")
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            raise ValueError("The email address is invalid")
 
-    def is_valid_email(self, email):
-        email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-        return re.match(email_regex, email) is not None
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+
+    def update(self, data):
+        if 'first_name' in data:
+            self.first_name = data['first_name']
+        if 'last_name' in data:
+            self.last_name = data['last_name']
+        if 'email' in data and re.match(r"[^@]+@[^@]+\.[^@]+", data['email']):
+            self.email = data['email']
+        super().update(data)
