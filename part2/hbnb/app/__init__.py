40caae7926906472e0f3ac24e263046b7f823fb3
scotty800 <scottyndanga02400@gmail.com>
2024-10-21 10:39:44 +0200

    jsut save

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 def create_app():
+    from app.api.v1.places import api as places_api
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Placeholder for API namespaces (endpoints will be added later)
+    api.add_namespace(places_api, path='/api/v1/places')
+    api.add_namespace(users_ns, path='/api/v1/users')
     # Additional namespaces for places, reviews, and amenities will be added later
 
     return app
