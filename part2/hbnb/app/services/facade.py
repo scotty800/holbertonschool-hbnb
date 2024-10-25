3dd72834b1810412ea16c9f064df7878f8451798
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-25 03:00:40 +0200

    add reviews

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
@@ -30,6 +31,7 @@ class HBnBFacade:
             raise ValueError("User not found")
 
         try:
+
             user.update(user_data)
         except ValueError as error:
             raise error
@@ -37,3 +39,35 @@ class HBnBFacade:
         self.save_user(user)
 
         return user
+
+
+    def create_review(self, review_data):
+        review = Review(**review_data)
+        self.review_repo.add(review)
+        return review
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return list(self.review_repo.get_all())
+
+    def get_reviews_by_place(self, place_id):
+        all_reviews = self.review_repo.get_all()
+
+    def update_review(self, review_id, review_data):
+        review = self.get_review(review_id)
+
+        if not review:
+            raise ValueError('Review not found')
+
+        if 'text' in review_data:
+            review.text = review_data['text']
+        if 'rating' in review_data:
+            review.rating = review_data['rating']
+
+        self.review_repo.update(review_id, review_data)
+        return review
+
+    def delete_review(self, review_id):
+        return self.review_repo.delete(review_id)
